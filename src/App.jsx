5cfc6600285a9453f94da6a545fa34773e74c47e
watchYourCache <watchYourCache@gmail.com>
2022-10-31 20:03:44 +0800

    feat: Improve profile description

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ export default function App() {
       <Profile
         imgUrl="https://cdn.discordapp.com/avatars/1005756074973990922/7f81c7a89fb3f800f172f65e50df1598.png?size=256"
         name="1nterp"
-        description="Gamer, developer and professional procrastinator"
+        description="A 13-year-old gamer, developer, and professional procrastinator"
       />
 
       <TechUsed />
